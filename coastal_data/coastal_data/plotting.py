5b3e43b176ee9ee65e88d9481ca535522d3b5795
Susann Aschenneller <s.aschenneller@utwente.nl>
2022-05-03 17:56:53 +0200

    better tracks plotting function

--- coastal_data/coastal_data/plotting.py
+++ coastal_data/coastal_data/plotting.py
@@ -2,34 +2,38 @@ import matplotlib.pyplot as plt
 from cartopy.feature import LAND
 import cartopy.crs as ccrs
 
-def plot_altimetry_tracks(lon, lat, save=False, *args):
-    # TODO:
-    # - fix colobar to show the real values and not something *8e2
-    # - include extent
-    # - documentation
-    # - better optional arguments (probably something like if len(args) == 3)
-    # - how to use the argument name in the function call? Like plotting.plot_altimetry_tracks(lon=ds.lon_20_ku, lat=ds.lat_20_ku, save=True, value=ds.range_ales_20_ku, title='test') to increase readability of code
-    # (also apply learned things to the prepare_S3_data function)
-    
-    value = args[0]
-    title = args[1]
+def plot_altimetry_tracks(lon, lat, save=False, value=None, title='', extent=None):
+    """
+    lon: array
+    lat: array
+    save: bool
+        True: saves the plot as .png
+    value : array
+        Vector with same length as lon and lat, adds colour-coding
+    title: string
+    extent: dictionary in the form of {'lon_min': x.x, 'lon_max': x.x, 'lat_min': x.x, 'lat_max': x.x}
+    """
     
     fig = plt.figure(1,figsize=(15,15))
+    #plt.ticklabel_format(useOffset=False) # want to see the real numbers on the y-axes, not something *8e5
+    
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.add_feature(LAND, edgecolor = 'darkgray', facecolor = "lightgray", zorder=1)
+    ax.gridlines(draw_labels=True, crs=ccrs.PlateCarree())
     
-    #wnse=[3,7,56,53]
-    #wnse=[5,6,54,53]
-    #ax.set_extent(wnse, crs=ccrs.PlateCarree())
+    if extent != None:
+        wnse = list(extent.values())
+        ax.set_extent(wnse, crs=ccrs.PlateCarree())
     
-    plot = plt.scatter(lon, lat, c = value/1000, marker ='+') # /1000 to get values in [km]
+    if value is None:
+        plot = plt.scatter(lon, lat, marker ='+')
+    else:
+        plot = plt.scatter(lon, lat, c = value, marker ='+')
+        cbar = plt.colorbar(plot, orientation="vertical")
+        cbar.ax.tick_params(labelsize=20)
+        cbar.set_label(label='[m]',size=20)
     
-    cbar = plt.colorbar(plot, orientation="vertical")
-    cbar.ax.tick_params(labelsize=20)
-    cbar.set_label(label='[km]',size=20)
-    
-    plt.title(title,fontsize=25)
-    
+    plt.title(title,fontsize=25)    
     plt.xticks(fontsize=20); # produces weird array output without ;
     plt.yticks(fontsize=20);
     
