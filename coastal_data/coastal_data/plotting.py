c4b112b75028cc7f59bea7572062a1a0cdfd66f9
Susann Aschenneller <s.aschenneller@utwente.nl>
2022-05-12 17:24:50 +0200

    scatter plot can now take several datasets, legend placing in time-series is more something to do by hand

--- coastal_data/coastal_data/plotting.py
+++ coastal_data/coastal_data/plotting.py
@@ -2,19 +2,20 @@ import matplotlib.pyplot as plt
 from cartopy.feature import LAND
 import cartopy.crs as ccrs
 
-def plot_altimetry_tracks(lon, lat, save=False, value=None, title='', extent=None):
+def plot_altimetry_tracks(lon, lat, save=False, value=None, title='', label='', extent=None):
     """
-    lon: array
-    lat: array
+    lon: tuple of arrays
+    lat: tuple of arrays
     save: bool
         True: saves the plot as .png
-    value : array
+    value : tuple of arrays
         Vector with same length as lon and lat, adds colour-coding
     title: string
+    label: string
     extent: dictionary in the form of {'lon_min': x.x, 'lon_max': x.x, 'lat_min': x.x, 'lat_max': x.x}
     """
     
-    fig = plt.figure(7495,figsize=(15,15))
+    fig = plt.figure(7495,figsize=(20,10))
     #plt.ticklabel_format(useOffset=False) # want to see the real numbers on the y-axes, not something *8e5
     
     ax = plt.axes(projection=ccrs.PlateCarree())
@@ -23,16 +24,20 @@ def plot_altimetry_tracks(lon, lat, save=False, value=None, title='', extent=Non
     
     if extent != None:
         wnse = list(extent.values())
-        ax.set_extent(wnse, crs=ccrs.PlateCarree())  # this line leads to kernel dying (8.5.22)
+        ax.set_extent(wnse, crs=ccrs.PlateCarree())
     
-    if value is None:
-        plot = plt.scatter(lon, lat, marker ='+')
-    else:
-        plot = plt.scatter(lon, lat, c = value, marker ='+')
+    for i in range(0, len(lon)):
+        if value is None:
+            plot = plt.scatter(lon[i], lat[i], marker ='+')
+        else:
+            plot = plt.scatter(lon[i], lat[i], c=value[i], marker='+')
+
+
+    if value != None:
         cbar = plt.colorbar(plot, orientation="vertical")
         cbar.ax.tick_params(labelsize=20)
-        cbar.set_label(label='[m]',size=20)
-    
+        cbar.set_label(label=label, size=20)
+            
     plt.title(title,fontsize=25)    
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
@@ -40,31 +45,42 @@ def plot_altimetry_tracks(lon, lat, save=False, value=None, title='', extent=Non
     if save == True:
         plt.savefig('altimetry_track', transparent=True)
         
-def plot_timeseries(x_data, y_data, title='', ylabel='', save=False):
+def plot_timeseries(x_data, y_data, title='', ylabel='', save=False, sym_y_bounds=True):
     """
-    x_data: array
-    y_data: dictionary in the form of {'data':(array1, array2, ...), 'legend':('legend1', 'legend2', ...)}
+    x_data: tuple of arrays
+    y_data: dictionary in the form of {'data':(array1, array2, ...), 'legend':('legend1', 'legend2', ...), 'color':('color1', 'color2', ...)}
     title: string
     ylabel: string
     save: bool
         True: saves the plot as .png
+    sym_y_bounds: bool
+        True: boundaries of y-axis are symmetrical (from -x to +x)
     """
     
     plt.figure(7496, figsize=(15,10))
+    all_bounds = [] # determine range of y-axis
     
     for i in range(0, len(y_data['data'])):
-        plt.plot(x_data, y_data['data'][i], '.-', label=y_data['legend'][i])
-        plt.legend()        
+        plt.plot(x_data[i], y_data['data'][i], '.-', color=y_data['color'][i], label=y_data['legend'][i])
+        #plt.legend(fontsize=18)
+        all_bounds.append(max(y_data['data'][i].max(), abs(y_data['data'][i].min())))
     
     plt.grid()
-    plt.legend(fontsize=20)
+    #plt.legend(fontsize=20, loc='lower right') # upper, lower, center
+    plt.legend(fontsize=20, loc='best', bbox_to_anchor=(0.5, -0.05, 0.5, 0.5))    
+    #plt.legend(fontsize=20)
+    #plt.figlegend(fontsize=20)
     
     plt.title(title,fontsize=25)    
-    plt.xticks(fontsize=20); # produces weird array output without ;
+    plt.xticks(fontsize=20);
     plt.yticks(fontsize=20);
     
     plt.xlabel('Time', fontsize=20)
     plt.ylabel(ylabel, fontsize=20)
+    
+    if sym_y_bounds == True:
+        bounds = max(all_bounds) + 0.1*max(all_bounds)
+        plt.ylim(top=bounds, bottom=-bounds)
     
     if save == True:
         plt.savefig('timeseries', transparent=True)
