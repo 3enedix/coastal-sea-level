aa1689cc56e7ca207fb935e7ff5cf567a6ff35be
Bene Aschenneller <s.aschenneller@utwente.nl>
2023-02-02 16:35:42 +0100

    moved functions to statistics module

--- coastal_data/statistics.py
+++ coastal_data/statistics.py
@@ -1,5 +1,31 @@
 import numpy as np
 
+def rms(ts):
+    return round(np.sqrt(np.nanmean(ts **2)),3)
+
+def std(ts): # Variation in one timeseries
+    err = ts - np.nanmean(ts)
+    return rms(err)
+
+def moving_average(ts, n):
+    '''
+    ts: array, timeseries
+    n: filterlength
+    '''
+    # fill the values that are cut off by the 'valid' option with nans:
+    if n % 2 == 0:
+        fill1 = np.full(int(n/2), np.nan)
+        fill2 = np.full(int(n/2)-1, np.nan)
+    else:
+        fill1 = np.full(int(n/2), np.nan)
+        fill2 = fill1
+        
+    filt = np.ones(n)/n
+    ts_sm_filt = np.convolve(ts, filt, 'valid')
+    ts_sm = np.concatenate((fill1, ts_sm_filt, fill2))
+    
+    return ts_sm
+
 def compute_trend(x, y):
     '''
     Estimates the trend of a linear function estimated with least squares through a given set of points.
@@ -36,4 +62,4 @@ def compute_trend(x, y):
     n = np.matmul(np.transpose(A), y) # A'l
     xs = np.linalg.solve(N,n)
     
-    return round(xs[0], 2)
+    return round(xs[0], 4)
