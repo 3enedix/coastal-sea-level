d4d793b327f26ec62fae687eab662be5a24ae991
Bene Aschenneller <s.aschenneller@utwente.nl>
2023-05-08 17:21:02 +0200

    function to convert decimal numbers to datetime

---
+++ coastal_data/helper_functions.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+def decimal_numbers_to_datetime(decim):
+    years = decim.astype('int')
+    doy = np.round(((decim - years) *12 *30.5),0).astype('int')
+
+    date = []
+    for i in range(0,len(doy)):
+        if doy[i] == 0:
+            day = (doy[i] + 1).astype('str')
+        else:
+            day = (doy[i]).astype('str')
+        day.rjust(3 + len(day), '0')
+        year = years[i].astype('str')
+        date.append(datetime.strptime(year + "-" + day, "%Y-%j").strftime("%m-%d-%Y"))
+
+    date = pd.to_datetime(date, utc=True)
+    return date
