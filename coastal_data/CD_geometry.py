add5d1d446ffac60de1db3a5da2ba4a5a3a96ff4
Bene Aschenneller <s.aschenneller@utwente.nl>
2024-06-18 09:25:40 +0200

    replaced vertices with simplices in scipy Delauney (vertices is deprecated)

--- coastal_data/CD_geometry.py
+++ coastal_data/CD_geometry.py
@@ -69,7 +69,7 @@ def concave_hull_alpha_shape(points, alpha=0.01):
     '''
     coords = get_coordinates(points)
     tri = Delaunay(coords)
-    triangles = coords[tri.vertices]
+    triangles = coords[tri.simplices]
     a = ((triangles[:,0,0] - triangles[:,1,0]) ** 2 + (triangles[:,0,1] - triangles[:,1,1]) ** 2) ** 0.5
     b = ((triangles[:,1,0] - triangles[:,2,0]) ** 2 + (triangles[:,1,1] - triangles[:,2,1]) ** 2) ** 0.5
     c = ((triangles[:,2,0] - triangles[:,0,0]) ** 2 + (triangles[:,2,1] - triangles[:,0,1]) ** 2) ** 0.5
