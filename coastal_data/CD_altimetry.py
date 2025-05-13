32cfe8fcfae4a83e6f4021702bf12f1d325ef932
Bene Aschenneller <s.aschenneller@utwente.nl>
2025-05-13 10:53:57 +0200

    move download of era5 mslp from data preparation to altimetry

--- coastal_data/CD_altimetry.py
+++ coastal_data/CD_altimetry.py
@@ -537,7 +537,40 @@ def plot_map(ax, epsg, centers, cell_stats, cell_stats_long, param, x_vec, y_vec
     plt.ioff()
 
     
+# ===================================================================================
+# IB correction
+# ===================================================================================
 
-
-
-
+def download_era5_mean_sea_level_pressure(path_output, fn):
+    import cdsapi
+    dataset = "reanalysis-era5-single-levels-monthly-means"
+    request = {
+        "product_type": ["monthly_averaged_reanalysis"],
+        "variable": ["mean_sea_level_pressure"],
+        "year": ["1992", "1993",
+            "1994", "1995", "1996",
+            "1997", "1998", "1999",
+            "2000", "2001", "2002",
+            "2003", "2004", "2005",
+            "2006", "2007", "2008",
+            "2009", "2010", "2011",
+            "2012", "2013", "2014",
+            "2015", "2016", "2017",
+            "2018", "2019", "2020",
+            "2021", "2022", "2023",
+            "2024", "2025"
+        ],
+        "month": [
+            "01", "02", "03",
+            "04", "05", "06",
+            "07", "08", "09",
+            "10", "11", "12"
+        ],
+        "time": ["00:00"],
+        "data_format": "netcdf",
+        "download_format": "unarchived"
+    }
+    
+    target = path_output + fn
+    client = cdsapi.Client()
+    client.retrieve(dataset, request, target)
